df85defaa81be058fdd778e440790fdbe3e922f4
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2015-11-03 09:39:27 +0100

    add unit test for coifwavf

---
+++ pyyawt/tests/test_coifwavf.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015 Holger Nahrstaedt
+from __future__ import division, print_function, absolute_import
+
+import os
+import numpy as np
+
+import sys
+import unittest
+import pyyawt
+
+
+class TestCoifwavf(unittest.TestCase):
+    def test_sum(self):
+        for N in np.arange(1,18):
+            w = pyyawt.coifwavf("coif" + str(N))
+            np.testing.assert_almost_equal(np.sum(w)-np.sqrt(2),0)
+
+
+if __name__ == '__main__':
+    # run_module_suite(argv=sys.argv)
+    unittest.main()
